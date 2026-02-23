619f1285826c74e0727b4296d45ad995a7cb515b
dimig230 <dimig@ucsd.edu>
2026-02-23 11:04:11 -0600

    pre procolizing commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# squirel
-Squishy Relic 
--- plotting/test.py
+++
@@ -1,8 +0,0 @@
-from utils import *
-import numpy as np
-import matplotlib.pyplot as plt
-from classy import Class
-
-# test script for whatever I'm doing
-z_array = np.logspace(4,-8,100)
-print(z_array,1/(1+z_array)**(-3)*asympt_scaling(1/(1+z_array),0.3,1e3))
